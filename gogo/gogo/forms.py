56766b36b721d817a04b60277636d49c1dc04500
Daniel Temple <dantdj@gmail.com>
2016-09-17 12:59:00 +0200

    Added sign-up form into template, should put into database

--- gogo/gogo/forms.py
+++ gogo/gogo/forms.py
@@ -1,19 +1,19 @@
 from django.forms import ModelForm
-from models import Traveller, Location, TravelPlan
+from .models import Traveller, Location, TravelPlan
 
 class TravellerForm(ModelForm):
     class Meta:
         model = Traveller
-        fields = ['traveller_ID', 'last_name', 'first_name', 'nationality', 'mobile_number', 'email', 'password', 'preferences']
+        fields = ['last_name', 'first_name', 'nationality', 'mobile_number', 'email', 'password', 'preferences']
 
 
 class LocationForm(ModelForm):
     class Meta:
         model = Location
-        fields = ['location_ID', 'country', 'city', 'name', 'language']
+        fields = ['country', 'city', 'name', 'language']
 
 
 class TravelPlanForm(ModelForm):
     class Meta:
         model = TravelPlan
-        fields = ['travel_plan_ID', 'travel_plan_name', 'traveller_ID', 'location_ID', 'date_from', 'date_to']
+        fields = ['travel_plan_name', 'date_from', 'date_to']
--- gogo/gogo/views.py
+++ gogo/gogo/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TravellerForm
 
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        form = TravellerForm(request.POST)
+        if form.is_valid():
+            data = form.save()
+            return redirect('/')
+    else:
+        form = TravellerForm()
+
+    return render(request, 'signup.html', {'form': form})
 
 def match(request):
     return render(request, 'match.html')
