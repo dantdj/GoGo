37dc5234641035968d1580994710af3deb77bd73
Daniel Temple <dantdj@gmail.com>
2016-09-17 06:10:55 +0200

    Initial database table setup, with fields

---
+++ gogo/gogo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class GoGoConfig(AppConfig): # Our app config class
+    name = 'gogo'
+    verbose_name = "GoGo: Connecting Travellers"
--- gogo/gogo/settings.py
+++ gogo/gogo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gogo.apps.GoGoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
